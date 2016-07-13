9de5fad0903449f3b4bd887a3d96648f299f3c6a
Elizabeth Fitzpatrick <email": null>
2016-07-13 16:21:14 -0400

    work on static_pgs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @recipe = current_user.recipes.build
 
+      @recipes = Recipe.where(user: current_user)
+    end
   end
 
   def about
