19abe86369c62fe153eaa615db042f72c5e8f4f2
Jeffrey Ng <email": null>
2016-07-13 15:08:20 -0400

    rebasing, started on creating new recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,23 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = user.recipe.build(recipe_params)
+
+    if @recipe.save
+
+    else
+
+    end
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :course, :directions, :description, :time, :difficulty)
+    end
 end
