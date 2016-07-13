190f25fa0429d68e26a71671fdd4b6afb87777df
Joel_L <plagueoffrogs@gmail.com>
2016-07-13 19:48:44 -0400

    Add user show page and basic styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,30 +2,31 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper
 
   def current_user?(user)
     user == current_user
   end
 
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: session[:user_id])
-    # elsif (user_id = cookies.signed[:user_id])
-    #   user = User.find_by(id: user_id)
-    #   if user && user.authenticated?(:remember, cookies[:remember_token])
-    #     log_in user
-    #     @current_user = user
-    #   end
-    end
-  end
+  # def current_user
+  #   if (user_id = session[:user_id])
+  #     @current_user ||= User.find_by(id: session[:user_id])
+  #   # elsif (user_id = cookies.signed[:user_id])
+  #   #   user = User.find_by(id: user_id)
+  #   #   if user && user.authenticated?(:remember, cookies[:remember_token])
+  #   #     log_in user
+  #   #     @current_user = user
+  #   #   end
+  #   end
+  # end
 
-  def log_in(user)
-    session[:user_id] = user.id
-  end
+  # def log_in(user)
+  #   session[:user_id] = user.id
+  # end
 
-  def logged_in?
-    !current_user.nil?
-  end
+  # def logged_in?
+  #   !current_user.nil?
+  # end
 
 
 end
