95c99b329d9fe194411400317de3120624ea10d1
Anthony Ciacci & Elizabeth Fitzpatrick & Jeffrey Ng & Joel Lambdin <pair+ciacci1234.efitz16.jeffreymng.joel-L@devbootcamp.com>
2016-07-13 15:41:16 -0400

    empty static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    # if logged_in?
-    #   @receipe = current_user.receipes.build
 
-    #   @receipes = Receipe.where(user: current_user)
-    # end
   end
 
   def about
