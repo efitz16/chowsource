1209d0d17b34a326f73fe82dd1ff30fd29f28f5e
Anthony Ciacci <ciacci1234@gmail.com>
2016-07-13 15:47:44 -0400

    create index page for ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,4 +2,11 @@ class IngredientsController < ApplicationController
   def index
     @ingredients = Ingredient.all
   end
+
+  def new
+    @ingredient = Ingredient.new(ingredient_params)
+  end
+
+  private
+  # ingredient_params
 end
