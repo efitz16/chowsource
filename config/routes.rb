0961b27d9169db1633a436cea42a7042d0ee065f
Anthony Ciacci <ciacci1234@gmail.com>
2016-07-13 15:09:10 -0400

    work in progress, adds ingredients controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root 'static_pages#home'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
@@ -7,7 +6,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
-
   resources :recipes
-
+  resources :ingredients
 end
