34e53565032f2fdaf9b6a5f8c540a8bb51da8030
Joel_L <plagueoffrogs@gmail.com>
2016-07-13 16:12:46 -0400

    Begin initial user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_many :recipes, dependent: :destroy
-  validates :username, presence: true, length: {maximum 75}
+  validates :username, presence: true, length: {maximum: 75}
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
   validates :email, presence: true,
-                    length: {maximum:255},
+                    length: {maximum: 255},
                     format: {with: /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z\d\-]+)+\z/i},
                     uniqueness: {case_sensitive: false}
   
