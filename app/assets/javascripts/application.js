e8d4ab683381f59ce5083a961ed60fc1dba8aad6
Elizabeth Fitzpatrick <email": null>
2016-07-15 16:17:06 -0400

    get buttons to work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,54 @@
 //= require jquery
 //= require bootstrap
 //= require jquery_ujs
-//= require_tree 
+//= require_tree
 
 $(document).ready(function(){
   $('.dropdown-toggle').dropdown();
-
 })
+
+$(document).ready(function(){
+
+  var length = $('.rating-button').length;
+
+    for (var i = 0; i < length; i++) {
+      if ($($('.rating-button')[i]).attr('id') <= $('#average').text().slice(-1)){
+        $($('.rating-button')[i]).css({'background-color': 'yellow'});
+        $($('.rating-button')[i]).addClass("stay");
+      }
+    }
+
+    // var mousedover = [];
+
+    // $('.rating-button').on("mouseover", function(e){
+    //   mousedover.push($(e.target).attr('id'));
+    // });
+
+    $('.rating-button').on("mouseover", function(e){
+      // if ($(e.target).attr('id') <= ) {
+        var button = $(e.target);
+        for (var i = 0; i < length; i++) {
+          if ($($('.rating-button')[i]).attr('id') <= button.attr('id')) {
+            $($('.rating-button')[i]).css({'background-color': 'yellow'});
+          }
+        }
+      // $(e.target).css({'background-color': 'yellow'});
+      });
+    $('.rating-button').on("mouseleave", function(e){
+      var button = $(e.target);
+      for (var i = 0; i < length; i++) {
+        if ($($('.rating-button')[i]).attr('id') > $('#average').text().slice(-1)) {
+          $($('.rating-button')[i]).css({'background-color': 'white'});
+        }
+
+      }
+
+
+      // if (!($(e.target).hasClass("stay"))) {
+      //     if ($(e.target).attr('id') > $('#average').text().slice(-1)){
+      //       $(e.target).css({'background-color': 'white'});
+      //     }
+      // }
+      });
+})
+
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,7 +2,7 @@ class RatingsController < ApplicationController
   def create
     @recipe = Recipe.find(params[:recipe_id])
     if logged_in?
-      @rating = Rating.new(recipe: @recipe, user: current_user, value: params[:value].to_i + 1)
+      @rating = Rating.new(recipe: @recipe, user: current_user, value: params[:value].to_i)
 
       @rating.save
     end
