18d758f88ae17f211c2f6acc7f55bdec2e381b87
Anthony Ciacci <ciacci1234@gmail.com>
2016-07-15 12:37:13 -0400

    create validations on front-end and back-end, also display errors

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,7 +4,13 @@ class Ingredient < ActiveRecord::Base
   has_many :food_preps, dependent: :destroy
   has_many :recipes, through: :food_preps
 
-  validates :ingredient_type, exclusion: { in: [""], message: "Cannot be blank" }
+  validate :check_blank
+
+  def check_blank
+    if ingredient_type.name.length == 0
+      errors.add(:ingredient_type, "can't be blank")
+    end
+  end
 
   def name
     self.ingredient_type.name
