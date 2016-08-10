2045bfd0e38cd265f4bdd33481f73f7ba02798af
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-08-10 15:58:24 -0400

    add rating validation

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,7 @@
 class RatingsController < ApplicationController
   def create
     @recipe = Recipe.find(params[:recipe_id])
+    binding.pry
     if logged_in?
       @rating = Rating.new(recipe: @recipe, user: current_user, value: params[:value].to_i)
 
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,5 +3,5 @@ class Rating < ActiveRecord::Base
   belongs_to :recipe
 
   validates_uniqueness_of :user, { scope: [:recipe], message: "a user can only rate this recipe one time" }
-  validates :value, presence: true
+  validates :value, presence: true, :inclusion => {:in => (1..5).to_a}
 end
